bb9335464ba87c158002dbe70a12ee974b077797
Ryan Warner <warner.ryan@gmail.com>
2019-09-04 13:03:18 -0400

    First attempt at webpack build

--- config/webpack.config.js/resolvers.js
+++ config/webpack.config.js/resolvers.js
@@ -4,11 +4,6 @@ module.exports = {
   extensions: ['.js', '.ejs', '.json', '.jsx', '.css'],
   modules: paths.resolveModules,
   alias: {
-    'app/api': `${paths.src}/api`,
-    'app/styles': `${paths.src}/styles/index`,
-    'app/ui-kit': `${paths.src}/components/AppUIKit`,
-    'app/utilities': `${paths.src}/Utilities`,
-    'app/analytics': `${paths.src}/Analytics`,
-    'app': `${paths.src}`
+    app: `${paths.src}`
   }
 }
---
+++ scripts/build.js
@@ -0,0 +1,19 @@
+const webpack = require('webpack')
+const rimraf = require('rimraf')
+const paths = require('../config/paths')
+const webpackConfig = require('../config/webpack.config.js')(process.env.NODE_ENV || 'development')
+
+const outputType = process.env.OUTPUT_TYPE
+const buildDir = outputType === 'library' ? paths.libraryBuild : paths.clientBuild
+console.log('buildDir', buildDir)
+const build = async () => {
+  rimraf.sync(buildDir)
+
+  webpack(webpackConfig, (error, stats) => {
+    console.log('webpack', error, stats)
+  })
+}
+
+build()
+
+module.exports = build
