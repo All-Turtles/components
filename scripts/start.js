ce167b2e20fdd8701d6bdf17cb1459d1afa3dbea
Ryan Warner <warner.ryan@gmail.com>
2019-09-05 16:58:01 -0400

    Add start script, instal babel runtime

---
+++ scripts/start.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack')
+const rimraf = require('rimraf')
+const paths = require('../config/paths')
+const webpackConfig = require('../config/webpack.config.js')(process.env.NODE_ENV || 'development')
+const WebpackDevServer = require('webpack-dev-server')
+
+const start = async () => {
+  rimraf.sync(paths.clientBuild)
+
+  const port = 3001
+  const host = '0.0.0.0'
+
+  webpackConfig.watch = true
+  const options = {
+    stats: { colors: true },
+    hot: true,
+    historyApiFallback: true,
+    disableHostCheck: true,
+    port,
+    contentBase: './public',
+    host
+  }
+
+  WebpackDevServer.addDevServerEntrypoints(webpackConfig, options)
+  const server = new WebpackDevServer(webpack(webpackConfig), options)
+  server.listen(port, host, (error) => {
+    if (error) {
+      console.log('Error', error)
+    }
+  })
+}
+
+start()
